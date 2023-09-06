f5698283f44cc360bce8069e5f4485a380b0efe3
danielpalmer <danielpalmer@localhost>
2023-09-06 13:58:19 -0500

    final studio 1 with Bernie Grunzweig and Daniel Palmer (messed up team)

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -9,12 +9,17 @@ public class Ordered {
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		boolean isOrdered = true;
 		System.out.println("Value for x?");
 		int x = in.nextInt();
 		System.out.println("Value for y?");
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		boolean first = x>y;
+		boolean second = y>z;
+		isOrdered = first && second;
+		System.out.println(isOrdered);
 
 	}
 
